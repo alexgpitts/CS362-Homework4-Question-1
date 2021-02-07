7fe1e8f9e70ba7bc28638f78611600d756b3eb0f
alexgpitts <alexgpitts@gmail.com>
2021-02-07 12:27:31 -0800

    simplified it
    I was doing a cuboid instead of a cube

--- test_volume_of_a_cube.py
+++ test_volume_of_a_cube.py
@@ -4,46 +4,18 @@ import volume_of_a_cube
 class TestCase(unittest.TestCase):
 
     #testing int values
-    def test_volume_of_a_cube1(self):
-        self.assertEqual(volume_of_a_cube.volume_of_cube(1, 4, 4), 16)
-
-    def test_volume_of_a_cube2(self):
-        self.assertEqual(volume_of_a_cube.volume_of_cube(4, 1, 4), 16)
-
-    def test_volume_of_a_cube3(self):
-        self.assertEqual(volume_of_a_cube.volume_of_cube(4, 4, 1), 16)
+    def test_volume_of_a_cube(self):
+        self.assertEqual(volume_of_a_cube.volume_of_cube(4), 64)
 
 
     #testing if float nums work
     def test_volume_of_a_cube_float(self):
-        self.assertEqual(volume_of_a_cube.volume_of_cube(4.0, 4.0, 4.0), 64.0)
+        self.assertEqual(volume_of_a_cube.volume_of_cube(4.0), 64.0)
 
 
-    #testing if it can tell when negative values are entered for length width or hight
-    #individually
-    def test_negative_length(self):
-        self.assertEqual(volume_of_a_cube.volume_of_cube(-1, 4, 4), -16)
-
-    def test_negative_width(self):
-        self.assertEqual(volume_of_a_cube.volume_of_cube(4, -1, 4), -16)
-    
-    def test_negative_hight(self):
-        self.assertEqual(volume_of_a_cube.volume_of_cube(4, 4, -1), -16)
-
-
-    #testing if it can tell when negative values are entered for length width or hight
-    #in combination
-    def test_negative_length_and_width(self):
-        self.assertEqual(volume_of_a_cube.volume_of_cube(-1, -5, 4), 20)
-
-    def test_negative_length_and_hight(self):
-        self.assertEqual(volume_of_a_cube.volume_of_cube(-1, 4, -4), 16)
-    
-    def test_negative_width_and_hight(self):
-        self.assertEqual(volume_of_a_cube.volume_of_cube(1, -5, -4), 20)
-    
-    def test_negative_all(self):
-        self.assertEqual(volume_of_a_cube.volume_of_cube(-1, -5, -4), -20)
+    #testing if it can tell when negative values are entered for edge
+    def test_negative_edge(self):
+        self.assertEqual(volume_of_a_cube.volume_of_cube(-4), 64)
 
 
 
--- volume_of_a_cube.py
+++ volume_of_a_cube.py
@@ -7,17 +7,16 @@ Description: This program simply calculates the
 volume of a cube with the function "volume_of_cube()"
 '''
 
-def volume_of_cube(length, width, hight):
+def volume_of_cube(edge):
 
     #convert inputs to ints
-    length = int(length)
-    width = int(width)
-    hight = int(hight)
+    edge = int(edge)
+    
     
     #if a negative num is detected, calculate error value and return it
-    if(length<1 or width<1 or hight<1):
+    if(edge<1):
         return -1
     
     #calculate errors if no negative values were entered
-    volume = length*width*hight
+    volume = edge**3
     return volume
